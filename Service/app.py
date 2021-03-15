7e7967f367990882539b2a35293bffc494b67ac0
dannielgloria <danniel.gloria@gmail.com>
2021-03-14 23:29:30 -0600

    databaseCredentials

--- Service/app.py
+++ Service/app.py
@@ -21,7 +21,7 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'u917498081_servicedb'
 app.config['MYSQL_DATABASE_PASSWORD'] = '$|OS8YaC/r5I'
 app.config['MYSQL_DATABASE_DB'] = 'u917498081_INEDATABASE'
-app.config['MYSQL_DATABASE_HOST'] = '31.220.104.219'
+app.config['MYSQL_DATABASE_HOST'] = 'www.taquitosoftware.com.mx'
 
 
 mysql.init_app(app)
